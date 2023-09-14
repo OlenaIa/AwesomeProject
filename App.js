2078e3462db2cfecebcee846432a3017457d4bfd
OlenaIa <121794911+OlenaIa@users.noreply.github.com>
2023-09-14 20:01:46 +0300

    Registration

--- App.js
+++ App.js
@@ -1,20 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import {RegistrationScreen} from './Screens/RegistrationScreen';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'RobotoRegular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'RobotoMedium': require('./assets/fonts/Roboto-Medium.ttf'),
+    'RobotoBold': require('./assets/fonts/Roboto-Bold.ttf'),
+
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <RegistrationScreen />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    fontFamily: 'RobotoRegular',
+    fontSize: 16,
+    fontStyle: 'normal',
+
   },
+  imageBack: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#212121',
+    textAlign: 'center',
+    fontSize: 30,
+    fontStyle: 'normal',
+    fontFamily: 'RobotoMedium',
+    letterSpacing: 0.3,
+    marginBottom: 33,
+  },
+  avatar: {
+    width: 120,
+    height: 120,
+    borderRadius: 16,
+  },
+  input: {
+    height: 50,
+    width: '100%',
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 16,
+    paddingBottom: 15,
+    backgroundColor: '#F6F6F6',
+    borderWidth: 1,
+    borderColor: '#E8E8E8',
+    color: '#212121',
+  },
+  button: {
+    width: 343,
+    paddingVertical: 16,
+    paddingHorizontal: 32,
+    borderRadius: 100,
+    backgroundColor: '#FF6C00',
+    
+marginBottom: 16,
+  },
+  textButton: {
+color: '#FFF',
+    textAlign: 'center',
+  },
+  visiblePassword: {
+    color: '#1B4371',
+
+  },
+  textUnderButton: {
+        color: '#1B4371',
+textShadowColor: 'rgba(0, 0, 0, 0.25)',
+textShadowOffset: {width: 0, height: 4},
+  textShadowRadius: 4,
+  }
 });
---
+++ Screens/RegistrationScreen.jsx
@@ -0,0 +1,110 @@
+import { StyleSheet, ImageBackground, Linking, Text, TextInput, TouchableOpacity, View, Pressable, Image } from 'react-native';
+import { styles } from '../App';
+import back from '../images/back.jpg';
+import addPhoto from '../images/addPhoto.svg';
+import BtnAddPhoto from '../images/add.svg'
+
+export const RegistrationScreen = () => {
+    return (
+        <View style={styles.container}>
+            <ImageBackground source={back} resizeMode="cover" style={styles.imageBack}>
+                <View style={stylesRegister.container}>
+                    <View style={stylesRegister.imgWrap}>
+                        <Image source={addPhoto}
+                            style={stylesRegister.imgAvatar} />
+                        <Pressable
+
+                    // onPress={onPressFunction}
+                    >
+  {/* <BtnAddPhoto width={25} height={25} /> */}
+</Pressable>
+                    </View>
+                    <Text style={styles.title}>Реєстрація</Text>
+                    <View style={stylesRegister.formWrap}>
+                        <TextInput
+                            style={styles.input}
+                            // onChangeText={onChangeNumber}
+                            // value={number}
+                            placeholder="Логін"
+                            keyboardType="default"
+                        />
+                        <TextInput
+                            style={styles.input}
+                            // onChangeText={onChangeNumber}
+                            // value={number}
+                            placeholder="Адреса електронної пошти"
+                            keyboardType="email-address"
+                        />
+                        <TextInput
+                            style={styles.input}
+                            // onChangeText={onChangeNumber}
+                            // value={number}
+                            placeholder="Пароль"
+                            keyboardType="visible-password"
+                        />
+                        <TouchableOpacity
+                            style={stylesRegister.touchWrapForm}
+                            onPress={() => Linking.openURL('#')}>
+                        <Text style={styles.visiblePassword}>
+                            Показати
+                        </Text>
+                    </TouchableOpacity>
+                    </View>
+                    
+                    <Pressable style={styles.button}
+
+                    // onPress={onPressFunction}
+                    >
+  <Text style={styles.textButton}>Зареєструватися</Text>
+</Pressable>
+                <TouchableOpacity onPress={() => Linking.openURL('#')}>
+                    <Text style={styles.visiblePassword}>
+                        Вже є акаунт? Увійти
+                    </Text>
+                    </TouchableOpacity>
+                    </View>
+                </ImageBackground>
+        </View>
+    )
+};
+
+const stylesRegister = StyleSheet.create({
+    container: {
+        position: 'relative',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        justifyContent: "flex-end",
+        paddingHorizontal: 16,
+        paddingTop: 92,
+        paddingBottom: 78,
+        width: '100%',
+        borderTopStartRadius: 25,
+        borderTopEndRadius: 25,
+    },
+    imgWrap: {
+        position: 'absolute',
+        top: -60,
+        width: 120,
+        height: 120,
+        backgroundColor: '#F6F6F6',
+        borderRadius: 16,
+    },
+    imgAvatar: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'contain',
+    },
+    formWrap: {
+        width: 343,
+
+        gap: 16,
+        marginBottom: 43,
+        position: 'relative',
+    },
+    touchWrapForm: {
+        position: 'absolute',
+        bottom: 31,
+        right: 16,
+    }
+});
